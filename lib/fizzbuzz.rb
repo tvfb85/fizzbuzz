eb2164f30ccbbfafa9b56d229b763715b33eeefb
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-12 12:30:09 +0000

    Fizzbuzz working

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
   it 'returns "fizz" for the number 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" for the number 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "16" for the number 16' do
+    expect(fizzbuzz(16)).to eq 16
   end
 end
